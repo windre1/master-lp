606ff5f5c3c61b2eb6ffc8903ca778f7f35ceb92
windre1 <winalislam@gmail.com>
2026-04-16 01:05:45 +0700

    update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,10 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase ENV belum diisi!");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
